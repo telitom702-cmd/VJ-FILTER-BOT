e0f190d0c32799b356eee14a35f41250ff804df9
telitom702-cmd <telitom702@gmail.com>
2026-09-07 23:08:05 +0600

    Update print statement from 'Hello' to 'Goodbye'

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@
 
 import logging, asyncio, os, re, random, pytz, aiohttp, requests, string, json, http.client
 from info import * 
-from imdb import Cinemagoer 
+import imdb 
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import enums
 from pyrogram.errors import *
@@ -22,7 +22,8 @@ logger.setLevel(logging.INFO)
 join_db = JoinReqs
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))")
 
-imdb = Cinemagoer('http') 
+import imdb
+imdb = imdb.Cinemagoer()
 TOKENS = {}
 VERIFIED = {}
 BANNED = {}
