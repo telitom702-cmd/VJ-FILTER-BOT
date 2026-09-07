704ef340df25f5669aad02a6d17f3dd2e2db4876
telitom702-cmd <telitom702@gmail.com>
2026-09-07 21:11:27 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@
 
 import logging, asyncio, os, re, random, pytz, aiohttp, requests, string, json, http.client
 from info import *
-from imdb import Cinemagoer 
+# from imdb import Cinemagoer 
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import enums
 from pyrogram.errors import *
