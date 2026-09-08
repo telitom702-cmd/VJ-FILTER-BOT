86552297de871253f8c6397ed11863225eae83bc
telitom702-cmd <telitom702@gmail.com>
2026-09-08 10:48:44 +0600

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/request_gruop')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/request_gruop')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Mosharrafroy') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/jacpotfilmm')
 
 # True Or False
